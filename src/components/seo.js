bc75abcff4dd536a2526ae97bc24507faf5c7419
Maciej Laskowski <19705838+malaskowski@users.noreply.github.com>
2023-04-28 20:42:26 +0200

    set twitter card to large image

--- src/components/seo.js
+++ src/components/seo.js
@@ -36,7 +36,7 @@ const Seo = ({ description, title, image, children }) => {
       <meta property="og:description" content={metaDescription} />
       <meta property="og:type" content="website" />
       <meta property="og:image" content={image} />
-      <meta name="twitter:card" content="summary" />
+      <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:title" content={title} />
       <meta name="twitter:description" content={metaDescription} />
       <meta name="twitter:image" content={image} />
